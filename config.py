8314392fe96bed7f05b98345fcdde59f2bd0ba2b
Marco Fuchs <marco.fuchs@live.de>
2021-11-28 00:41:17 +0100

    Add initial python code and requirements

---
+++ config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+myenergi = {
+    'user': 'myenergi hub serial no.',
+    'password': 'myenergi password',
+    'zappi_sno': 'zappi serial no.',
+    'status_url': 'https://s18.myenergi.net/cgi-jstatus-*',
+    'zappi_url': 'https://s18.myenergi.net/cgi-jdayhour-Z',
+    'data_target_path': '/path/to/write/the/csv'
+}
---
+++ zappi.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import config as cfg
+import datetime
+import requests
+import logging
+
+from   requests.auth import HTTPDigestAuth
+
+
+CURRENT_DATE = datetime.datetime.today()
+
+DAY = f'{CURRENT_DATE.day}'
+MONTH = f'{CURRENT_DATE.month}'
+YEAR = f'{CURRENT_DATE.year}'
+USERNAME    = cfg.myenergi['user']
+PASSWORD    = cfg.myenergi['password']
+STATUS_URL  = cfg.myenergi['status_url']
+ZAPPI_SNO   = cfg.myenergi['zappi_sno'] 
+CSV_FILE    = cfg.myenergi['data_target_path'] + f'{YEAR}-{MONTH}.csv'
+daily_statistic_url = cfg.myenergi['zappi_url'] + cfg.myenergi['zappi_sno'] + '-' + f'{YEAR}-{MONTH}-{DAY}'
+
+#https://s18.myenergi.net/cgi-jdayhour-Znnnnnnnn-YYYY-MM-DD
+
+#function to access the server using a parsed URL 
+def access_server(url_request):
+    headers = {'User-Agent': 'Wget/1.14 (linux-gnu)'}
+    r = requests.get(url_request, headers = headers, auth=HTTPDigestAuth(USERNAME, PASSWORD), timeout=10)
+    if (r.status_code == 200):
+        print ("") #"Login successful..") 
+    elif (r.status_code == 401):
+        print ("Login unsuccessful!!! Please check USERNAME, PASSWORD or URL..")
+        quit()
+    else:
+        logging.info("Login unsuccessful, returned code: " + r.status_code)
+        quit()
+    #print (r.json())
+    return r.json()
+
+#function to calculate the daily total and return the result
+def generate_daily_total():
+    # initialize the variables
+    sum_h1d = 0
+    sum_h2d = 0
+    sum_h3d = 0
+    total = 0
+    response_data = access_server(daily_statistic_url)
+    for i in response_data[f'U{ZAPPI_SNO}']:
+        for key in i.keys():
+            if key == 'h1d':
+                sum_h1d = sum_h1d + i[key]
+            if key == 'h2d':
+                sum_h2d = sum_h2d + i[key]
+            if key == 'h3d':
+                sum_h3d = sum_h3d + i[key]
+    # calculate the total kWh from the joule values of the three phases of the zappi wallbox
+    total = (sum_h1d + sum_h2d + sum_h3d) / 3600000
+    return total
+
+
+#write the daily total to a .csv file
+def write_daily_total():
+    total = generate_daily_total()
+
+    #only write if there is a value > 0 kWh
+    if total > 0:
+        with open(CSV_FILE, 'a') as f:
+            f.write(f'{DAY},{MONTH},{YEAR},' + str(total) + '\n')
+        f.close()
+
+
+if __name__ == "__main__":
+    write_daily_total()
